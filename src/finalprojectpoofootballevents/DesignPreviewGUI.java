6ca5fd6294c6e8d40ea834193e15aa7f72a6ba14
Adhisson <Adhisson@Adhisson>
2021-09-06 18:31:13 -0500

    commmit de prueba

--- src/finalprojectpoofootballevents/DesignPreviewGUI.java
+++ src/finalprojectpoofootballevents/DesignPreviewGUI.java
@@ -18,6 +18,7 @@ public class DesignPreviewGUI extends javax.swing.JFrame {
         initComponents();
     }
 
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
