249438cbb65a09972fa3dda0e264e672b855b840
Daniela <daniela.roman@epn.edu.ec>
2021-09-19 13:18:38 -0500

    Creacion de clases e Interfaz

---
+++ src/finalprojectpoofootballevents/Estadio.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectpoofootballevents;
+
+/**
+ *
+ * @author Daniela Roman
+ */
+public class Estadio {
+    
+    private String capacidad;
+    private String localidad;
+    private String ciudad;
+    private String nombreEstadio;
+    
+}
---
+++ src/finalprojectpoofootballevents/Persona.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalprojectpoofootballevents;
+
+/**
+ *
+ * @author Daniela Roman
+ */
+public class Persona {
+    
+    protected String nombre;
+    protected String apellido;
+    protected String numCedula;
+    protected int edad;
+    
+}
