284ccf91f52f857d18da768154a47a07e4d3b5b0
Adhisson <Adhisson@Adhisson>
2021-09-18 21:24:37 -0500

    PN

--- src/finalprojectpoofootballevents/FinalProjectPOOFootballEvents.java
+++ src/finalprojectpoofootballevents/FinalProjectPOOFootballEvents.java
@@ -16,6 +16,8 @@ public class FinalProjectPOOFootballEvents {
      */
     public static void main(String[] args) {
         // TODO code application logic here 
+        // prueba user
+        
     }
    
 }
